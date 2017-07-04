910154c4f7578f567b436b8a768ab117fe51da77
vhanschristian <vhanschristian@gmail.com>
2017-07-04 13:34:45 +0700

    comment

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -220,10 +220,10 @@ class Main extends CI_Controller {
 					{
 						redirect("index.php/main/index");
 					}
-					else
-					{
-						redirect("index.php/main/checkout1");
-					}
+					// else
+					// {
+					// 	redirect("index.php/main/checkout1");
+					// }
 				}
 				
 				
